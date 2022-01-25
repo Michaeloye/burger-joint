7f93aa0c764d7acbe4868b6403395330a4ae2315
michaeloye <michaeloye23@gmail.com>
2022-01-25 16:31:50 +0100

    Add new toppings

---
+++ src/components/Toppings.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  // the transition is placed in the object it is to applied to
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: "spring", delay: 0.5 },
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
+};
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
+const Toppings = ({ addTopping, burger }) => {
+  let toppings = [
+    "beef",
+    "peppers",
+    "onions",
+    "olives",
+    "extra cheese",
+    "tomatoes",
+    "chicken",
+  ];
+
+  return (
+    <motion.div
+      className="toppings container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h3>Step 2: Choose Toppings</h3>
+      <ul>
+        {toppings.map((topping) => {
+          let spanClass = burger.toppings.includes(topping) ? "active" : "";
+          return (
+            <motion.li
+              key={topping}
+              onClick={() => addTopping(topping)}
+              // originX makes the animation relative to its initial x position
+              whileHover={{ scale: 1.3, color: "#f8e112", originX: 0 }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <span className={spanClass}>{topping}</span>
+            </motion.li>
+          );
+        })}
+      </ul>
+
+      <Link to="/order">
+        <motion.button variants={buttonVariants} whileHover="hover">
+          Order
+        </motion.button>
+      </Link>
+    </motion.div>
+  );
+};
+
+export default Toppings;
