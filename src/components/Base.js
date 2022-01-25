8e7c50458948f65100c06d19918daeecf5c9ed8b
michaeloye <michaeloye23@gmail.com>
2022-01-25 16:13:57 +0100

    Add bases; containing list of base choice

---
+++ src/components/Base.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  // the transition is placed in the object it is to applied to
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: "spring", delay: 0.5 },
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: { type: "spring", stiffness: 120 },
+  },
+};
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
+const Base = ({ addBase, burger }) => {
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
+
+  return (
+    <motion.div
+      className="base container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h3>Step 1: Choose Your Bun</h3>
+      <ul>
+        {bases.map((base) => {
+          let spanClass = burger.base === base ? "active" : "";
+          return (
+            <motion.li
+              key={base}
+              onClick={() => addBase(base)}
+              // originX makes the animation relative to its initial x position
+              whileHover={{ scale: 1.3, color: "#f8e112", originX: 0 }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <span className={spanClass}>{base}</span>
+            </motion.li>
+          );
+        })}
+      </ul>
+
+      {burger.base && (
+        <motion.div className="next" variants={nextVariants}>
+          <Link to="/toppings">
+            <motion.button variants={buttonVariants} whileHover="hover">
+              Next
+            </motion.button>
+          </Link>
+        </motion.div>
+      )}
+    </motion.div>
+  );
+};
+
+export default Base;
